04a3953b7657e7adf8c4aaab62869b9d2073e1b4
Ryota Higashi <koromay@gmail.com>
2020-11-10 15:25:09 +0900

    1st commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by 東　良太 on 2020/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
